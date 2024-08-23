38eeec8d8f26c9df640a61dd8ea4a38510e74706
mrtnGLSR <143207442+mrtnGLSR@users.noreply.github.com>
2024-08-23 16:43:32 +0200

    comit 3

--- code/API.py
+++ code/API.py
@@ -1,5 +1,22 @@
 # api for the game
 
+# modules
+from player import player
+
 # define chunks
 class chunk:
-    
+    def __init__(self, chunkContent):
+    self.chunkContent = chunkContent
+
+class solide:
+    def __init__(self, form, position, isSolide)
+    self.form = form
+    self.position = form
+    self.isSolide = isSolide
+
+class entity(solide):
+    def __init__(self)
+
+class player:
+    def __init__(self, health)
+    self.health = health
---
+++ code/nodes.py
@@ -0,0 +1,10 @@
+# add nodes
+
+# modules
+from api import solide
+
+# define wall
+class wall(solide):
+    def __init__(self)
+    form = [(-0.5, 0.5), (0.5, 0.5), (-0.5, -0.5), (0.5, -0.5)]
+    
