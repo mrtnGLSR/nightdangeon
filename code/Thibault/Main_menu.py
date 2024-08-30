937cc65fbe6bbb19a106ebe9d2c6c1c3066d8383
Thibault Le Normand <pw74epq@eduvaud.ch>
2024-08-30 14:04:21 +0200

    main menu V1

--- code/Thibault/Main_menu.py
+++ code/Thibault/Main_menu.py
@@ -2,13 +2,15 @@ import pygame
 import pygame.freetype
 from pygame.sprite import Sprite
 from enum import Enum
-
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
+import pygame_widgets
 # Variables
 running = True
 RED = (178, 34, 34)
 WHITE = (255, 255, 255)
 game_state = ""
-
+vol = 0.5 # Variable volume
 # Initialisation de Pygame
 pygame.init()
 screen_width = 1040
@@ -16,6 +18,12 @@ screen_height = 798
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Night Dungeon")
 
+# Lancement de la musique
+pygame.mixer.init()
+lobby_music = pygame.mixer.Sound('./music/SM_Lobby.mp3')
+lobby_music.play()
+
+lobby_music.set_volume(vol)
 # Chargement de l'image d'arrière-plan
 background_image = pygame.image.load("./img/bg.png").convert()
 bg_width = background_image.get_width()
@@ -25,7 +33,7 @@ scroll_speed = 0.03
 
 # functions
 def create_surface_with_text(text,font_size, text_rgb, bg_rgb):
-    font = pygame.freetype.Font("./fonts/Ancient Medium.ttf", font_size) # creation font variable
+    font = pygame.freetype.Font(("." + "/fonts/OwreKynge.ttf"), font_size) # creation font variable
     _, text_rect = font.render(text=text, fgcolor=text_rgb)     # Render the text to get its dimensions
     surface = pygame.Surface((text_rect.width, text_rect.height), pygame.SRCALPHA) # Create a surface with dimensions to fit the text and enable transparency
     text_rect.center = (text_rect.width // 2, text_rect.height // 2) # Center the text on the surface
@@ -34,7 +42,6 @@ def create_surface_with_text(text,font_size, text_rgb, bg_rgb):
 
 class UIElement(Sprite):
     def __init__(self, center_position, text, font_size, bg_rgb, text_rgb, action=None):
-        super().__init__()
         self.mouse_over = False
         default_image = create_surface_with_text(text, font_size, text_rgb, bg_rgb)
         highlight_image = create_surface_with_text(text, font_size * 1.2, text_rgb, bg_rgb)
@@ -118,25 +125,37 @@ def title_screen(screen):
 def options_screen(screen):
     btn_return = UIElement(center_position=(70, 700), font_size=30, bg_rgb=WHITE, text_rgb=WHITE, text='Return', action=GameState.TITLE)
     Title = UIElement(center_position=(520, 200), font_size=60, bg_rgb=WHITE, text_rgb=WHITE, text='Options')
-
+    music_text = UIElement(center_position=(520, 250), font_size=40, bg_rgb=WHITE, text_rgb=WHITE, text='Music')
+    slider = Slider(screen, 450, 300, 150, 15, min=0, max=100, step=1,colour=(255, 255, 255),  handleColour=(89, 110, 127))
     while running:
         mouse_up = False
-        for event in pygame.event.get():
+        events = pygame.event.get()  # Récupérer tous les événements
+        pygame_widgets.update(events)  # Mettre à jour les widgets avec tous les événements
+        
+        for event in events:
             if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                 mouse_up = True
+                
             if event.type == pygame.QUIT:
                 return GameState.QUIT
 
         scrolling_bg()
-
+        
+        # Mettre à jour et dessiner les autres éléments de l'UI
         Title.update((0, 0), None)
+        music_text.update((0, 0), None)
         Title.draw(screen)
+        music_text.draw(screen)
+        lobby_music.set_volume((slider.getValue() / 100))
         ui_action = btn_return.update(pygame.mouse.get_pos(), mouse_up)
         if ui_action is not None:
             return ui_action
-
         btn_return.draw(screen)
+        # Affichage du slider
+        slider.draw()
         pygame.display.flip()
+
+        
 
 def main():
     global running
@@ -149,5 +168,6 @@ def main():
             game_state = options_screen(screen)
         elif game_state == GameState.QUIT:
             running = False
+         
 
 main()
