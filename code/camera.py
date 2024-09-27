74409e13a79d07fdee15a64c11ffa6e60ab1deb6
mrtnGLSR <martin@famille-glauser.ch>
2024-09-27 16:52:54 +0200

    comit 27.09.2024
    commencement de l'affichage utilisateur
    - définition de la camera
    - définition de position relative
    - commencement de la détéction des objets à charger

--- code/camera.py
+++ code/camera.py
@@ -1,5 +1,6 @@
 import pygame
 camera = pygame.Rect(0,0,0,0)
+viewDistance = 4
 
 def create_screen(width, heigh, title):
     pygame.display.set_caption(title)
@@ -8,3 +9,38 @@ def create_screen(width, heigh, title):
     camera.width = width
     camera.height = heigh
     return screen
+
+# debug
+class DebugClass():
+    def __init__(self, position):
+        self.position = position
+        self.camera = Camera(self)
+    
+    def refresh(self):
+        print("salut")
+    #    self.camera.refresh(5)
+
+# classe définisant la caméra du jeu
+class Camera():
+    def __init__(self, player):
+        self.player = player
+    
+    # fonction de rafraichissement
+    def refresh(self, distance):
+        position = self.player.position
+
+        # définition du bloque sur lequel se situe le joueur
+        playerBlock = [position[0] // 1, position[1] // 1]
+
+        # définir la position relative des bloques à charger
+        chargedBlockList = []
+        for x in range(viewDistance * 2 + 1):
+            for y in range(viewDistance* 2 + 1):
+                chargedBlockList.append([playerBlock[0] - viewDistance + x, playerBlock[1] - viewDistance + y])
+
+        print(chargedBlockList)
+
+debugPlayer = DebugClass
+
+debugPlayer.refresh()
+
--- code/nodes.py
+++ code/nodes.py
@@ -2,33 +2,34 @@
 
 # définition des objets affichés
 class Solide:
-    def __init__(self, draw, position, walkable, texture):
+    def __init__(self, draw, position, walkable, texture, solidID):
         self.draw = draw
         self.position = position
         self.walkable = walkable
         self.texture = texture
+        self.solidID = solidID
 
 
 # define wall
 class Wall(Solide):
-    def __init__(self, texture):
-        Solide.__init__(self, [-0.5, 0.5, 0.5, -0.5], False, False, texture)
+    def __init__(self, texture, solidID):
+        Solide.__init__(self, [-0.5, 0.5, 0.5, -0.5], False, False, texture, solidID)
 #self, draw, position, walkable, texture
 
 
 # deifne floor
 class Floor(Solide):
-    def __init__(self, texture):
-        super().__init__([-0.5, 0.5, 0.5, -0.5], False, True, texture)
+    def __init__(self, texture, solidID):
+        super().__init__([-0.5, 0.5, 0.5, -0.5], False, True, texture, solidID)
 
 
 # brick define brick wall
 class BrickWall(Wall):
     def __init__(self):
-        Wall.__init__(self, False)
+        Wall.__init__(self, './img/brick-floor.jpg', 1)
 
 
 # brick define brick floor
 class BrickFloor(Floor):
     def __init__(self):
-        Floor.__init__(self, False)
+        Floor.__init__(self, './img/brick-wall.jpg', 0)
