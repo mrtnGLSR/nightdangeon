9b4c0c346a85fc728105dc8a045ef209dd205c2e
sayte <sayte@fedora.fritz.box>
2024-10-22 19:55:13 +0200

    1.0

--- code/camera.py
+++ code/camera.py
@@ -1,16 +1,172 @@
 # import pygame
 from mapgen import *
 from screen import *
+import os
 
 print("chargement de la camera ...", end = '')
 
 camera = pygame.Rect(0,0,0,0)
 viewDistance = 4
 zoom = 11
+#Variables
+walkCount = 0 # walkcount is the number of the frame during the animations for the walk
+attackCount = 0 # walkcount is the number of the frame during the animations for the attack
+up = False
+down = False
+right = False
+left = False
+attack = False
+dieCount = 0
+lifeState = 3
+# 1=up, 2=down, 3=right, 4=left
+last_movement = 2 # set default orientation
+# Dictionary
+image_cords = {
+    'y' : ((screen.get_height()) / 2) - 82.5,# position of the player in height ([window height] / 2 - [Player image size /2])
+    'x' : ((screen.get_width()) / 2) - 55 # position of the player in width ([window width] / 2 - [Player image size /2])
+}
 
+
+# Load sfx
+sword_sfx = pygame.mixer.Sound('./sfx/sword_avoid_slash.mp3')
+die_sfx = pygame.mixer.Sound('./sfx/die_SFX.mp3')
+
+# Lists
+walk_up = []
+walk_down = []
+walk_right = []
+walk_left = []
+walk_static = []
+attack_right = []
+attack_left = []
+attack_down = []
+attack_up = []
+heath = []
+die = []
 # constants
 # charger les images
 images = {"brickWall":pygame.image.load('./img/brick-wall.png'), "brickFloor":pygame.image.load('./img/brick-floor.png', ), "test-player":pygame.image.load('./img/Test-cub.png')}
+# This for load all images needed to make the animations
+class LoadSprites:
+    def __init__(self, position, orientation, list_add, range_number, size):
+        super().__init__()
+        for i  in range(range_number):
+            i += 1
+            image = pygame.image.load(os.path.join('./img', f'Player_{position}_{orientation}_{i}.png'))
+            image = pygame.transform.scale(image, size)
+            list_add.append(image)
+            
+# All class calls
+LoadSprites('run', 'up', walk_up, 6, (110, 165))
+LoadSprites('run', 'down', walk_down, 6, (110, 165))
+LoadSprites('run', 'left', walk_left, 6, (110, 165))
+LoadSprites('run', 'right', walk_right, 6, (110, 165))
+LoadSprites('attack', 'right', attack_right, 11, (110, 165))
+LoadSprites('attack', 'left', attack_left, 11, (110, 165))
+LoadSprites('attack', 'down', attack_down, 6, (110, 165))
+LoadSprites('attack', 'up', attack_up, 3, (110, 165))
+LoadSprites('static', 'up', walk_static, 1, (110, 165))
+LoadSprites('static', 'down', walk_static, 1, (110, 165))
+LoadSprites('static', 'right', walk_static, 1, (110, 165))
+LoadSprites('static', 'left', walk_static, 1, (110, 165))
+LoadSprites('die', 'down', die, 2, (110, 165))
+LoadSprites('die', 'floor', die, 1, (165, 110))
+class LifeSprite():
+    def __init__(self, name):
+        super().__init__()
+        image_hearth = pygame.image.load(os.path.join('./img', f'{name}.png'))
+        image_hearth = pygame.transform.scale(image_hearth, (60, 60))
+        heath.append(image_hearth)
+LifeSprite('hearth')
+LifeSprite('death_hearth')
+
+
+def redrawGameWindow(direction_up,direction_down,direction_left,direction_right,attack):
+    global walkCount, last_movement, attackCount, dieCount
+    # Apply a background color
+    if lifeState == 1:
+        screen.blit(heath[0], (0,0))
+        screen.blit(heath[1], (40,0))
+        screen.blit(heath[1], (80,0))
+    if lifeState == 2:
+        screen.blit(heath[0], (0,0))
+        screen.blit(heath[0], (40,0))
+        screen.blit(heath[1], (80,0))
+    if lifeState == 3:
+        screen.blit(heath[0], (0,0))
+        screen.blit(heath[0], (40,0))
+        screen.blit(heath[0], (80,0))
+    # if the frame is over 7 the variable is reset
+    if walkCount + 1 >= 7:
+        walkCount = 0
+    # if the frame is over 11 the variable is reset
+    if attackCount + 1 >= 11:
+        attackCount = 0
+    if attack:
+        if last_movement == 1: # if the last movement is up
+            if attackCount +1 >=4: # if the frame is over 3 the variable is reset
+                attackCount = 0
+            pygame.time.delay(160) # Delay for the sync of the animations
+            if attackCount == 2: # if the animation is in the middle 
+                sword_sfx.play()
+            screen.blit(attack_up[attackCount], (image_cords['x'],image_cords['y'])) # Draw the animations
+            attackCount += 1
+        if last_movement == 2:# if the last movement is down
+            if attackCount + 1 >=7: # if the frame is over 7 the variable is reset
+                attackCount = 0
+            pygame.time.delay(80)# Delay for the sync of the animations
+            if attackCount == 3: # if the animation is in the middle 
+                sword_sfx.play()
+            screen.blit(attack_down[attackCount], (image_cords['x'],image_cords['y'])) # Draw the animations
+            attackCount += 1
+       
+        if last_movement == 3:# if the last movement is right
+            if attackCount == 6:# if the animation is in the middle 
+                sword_sfx.play()
+            screen.blit(attack_right[attackCount], (image_cords['x'],image_cords['y']))# Draw the animations
+            attackCount += 1
+       
+        if last_movement == 4:# if the last movement is left
+            if attackCount == 6:# if the animation is in the middle
+                sword_sfx.play()
+            screen.blit(attack_left[attackCount], (image_cords['x'],image_cords['y']))# Draw the animations
+            attackCount += 1
+    elif lifeState == 0:
+        pygame.time.delay(500)
+        if dieCount != 2:
+            screen.blit(die[dieCount], (image_cords['x'],image_cords['y']))
+            die_sfx.play() 
+        if dieCount == 2:
+            screen.blit(die[dieCount], (image_cords['x']-55,image_cords['y']+82.5))
+        if dieCount < 2:
+            dieCount += 1
+    elif direction_up:  # If we are facing up
+        screen.blit(walk_up[walkCount], (image_cords['x'],image_cords['y'])) 
+        walkCount += 1
+        last_movement = 1 
+    elif direction_down: # If we are facing down
+        screen.blit(walk_down[walkCount], (image_cords['x'],image_cords['y']))
+        walkCount += 1 
+        last_movement = 2
+    elif direction_right: # If we are facing right
+        screen.blit(walk_right[walkCount], (image_cords['x'],image_cords['y']))
+        walkCount += 1 
+        last_movement = 3
+    elif direction_left: # If we are facing left
+        screen.blit(walk_left[walkCount], (image_cords['x'],image_cords['y']))
+        walkCount += 1 
+        last_movement = 4
+    
+    else:
+        if attack == False:  # If we are facing static
+            if last_movement == 1:# last direction the character move
+                screen.blit(walk_static[0], (image_cords['x'],image_cords['y']))
+            elif last_movement == 2:
+                screen.blit(walk_static[1], (image_cords['x'],image_cords['y']))
+            elif last_movement == 3:
+                screen.blit(walk_static[2], (image_cords['x'],image_cords['y'])) 
+            elif last_movement == 4:
+                screen.blit(walk_static[3], (image_cords['x'],image_cords['y']))
 # redimenssionner les images
 for i in images:
     images[i] = pygame.transform.scale(images[i], (10 * zoom, 10 * zoom))
@@ -20,11 +176,10 @@ class Camera():
     def __init__(self, player):
         self.player = player
     # fonction de rafraichissement
-    def refresh(self, position = False):
+    def refresh(self, direction_up,direction_down,direction_left,direction_right, attack, position = False):
         # la position de la caméra est la même que le joueur si elle n'est pas précisée
         if not position:
             position = self.player.position
-
         # définition du bloque sur lequel se situe le joueur
         playerBlock = [position[0] // 1, position[1] // 1]
 
@@ -36,7 +191,7 @@ class Camera():
                 chargedBlockList.append([position[0] // 1 + x - 1 - viewDistance, position[1] // 1 + y - 1 - viewDistance])
         
         screen.fill((0, 0, 0))
-
+        
         # charger les images sur l'écran
         index = 0
         for i in chargedBlockList:
@@ -45,6 +200,7 @@ class Camera():
             except:
                 pass
             index += 1
-            screen.blit(images["test-player"], [40 * zoom, 40 * zoom])
+        redrawGameWindow(direction_up,direction_down,direction_left,direction_right, attack)
         pygame.display.flip()
+
 print(" fait")
--- code/game.py
+++ code/game.py
@@ -13,7 +13,11 @@ from time import time
 clear_color = (0, 0, 0)
 # Variable de la boucle
 running = True
-
+up = False
+down = False
+right = False
+left = False
+attack_state = False
 # Définition des types de tuiles, avec une image et une propriété 
 # tile_kinds = [
 #     TileKind('floor', './img/brick-floor.jpg', False),  # Sol 
@@ -30,7 +34,6 @@ GenMap()
 print("startpoint:")
 print(startPoint)
 player = Player([3.6,4])
-
 # Chargement de la carte 
 # Taille des tuiles = 80 pixels
 
@@ -48,14 +51,64 @@ while running:
     
     # for s in sprites:  # Parcourt tous les sprites du jeu
     #     s.draw(screen)  # Dessine chaque sprite sur l'écran
-    
+    # add keys event
+    keys = pygame.key.get_pressed()
+    if (keys[pygame.K_w] or keys[pygame.K_UP]) and (keys[pygame.K_s] or keys[pygame.K_DOWN]):
+        # Static if up and down is pressed in the same time
+        up = down = right = left = False
+        walkCount = 0
+        
+    elif (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and (keys[pygame.K_a] or keys[pygame.K_LEFT]):
+        # Static if right and left is pressed in the same time
+        up = down = right = left = False
+        walkCount = 0
+        
+    else:
+        if keys[pygame.K_w] or keys[pygame.K_UP]: # if the keys to go up are pressed
+            up = True
+            right = False
+            down = False
+            left = False
+            attack_state = False
+        elif keys[pygame.K_s] or keys[pygame.K_DOWN]:  # if the keys to go down are pressed
+            up = False
+            right = False
+            down = True
+            left = False
+            attack_state = False
+        elif keys[pygame.K_d] or keys[pygame.K_RIGHT]: # if the keys to go right are pressed
+            up = False
+            right = True
+            down = False
+            attack_state = False
+            left = False
+        elif keys[pygame.K_a] or keys[pygame.K_LEFT]: # if the keys to go left are pressed
+            up = False
+            right = False
+            down = False
+            left = True
+            attack_state = False
+        elif keys[pygame.K_o]:
+            lifeState = 0
+        else:
+            up = right = down = left= attack_state = False # if no keys is pressed he was static
+            walkCount = 0
+        if event.type == pygame.MOUSEBUTTONDOWN: # if mouse boutton is pressed
+            attack_state = True
 
-    player.refresh()
+    player.refresh(direction_up=up,direction_down=down,direction_left=left,direction_right=right, attack=attack_state)
     # player.move(0.1, 0)
     #pygame.display.flip()  # Rafraîchit l'écran, montre tout ce qui a été dessiné
     # Pause pour limiter la vitesse d'exécution de la boucle à 0.1s
     while time() < startTime + 0.05:
         pass
-    player.move(0, 0.1)
+    if keys[pygame.K_w] or keys[pygame.K_UP]:
+        player.move(0, -0.2)
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        player.move(0, 0.2)
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        player.move(-0.2, 0)
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        player.move(0.2, 0)
     print("player-position" + str(player.position))
 pygame.quit()
